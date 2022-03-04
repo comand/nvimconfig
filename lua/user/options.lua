9c7f1c930fbcf959d0813cc42db577057c932573
Cory Omand <comand@pixar.com>
2022-03-04 15:07:23 -0800

    First cut from Neovim-from-scratch

---
+++ lua/user/options.lua
@@ -0,0 +1,44 @@
+local options = {
+    backup = false,
+    breakindent = true,
+    clipboard = 'unnamedplus',
+    cmdheight = 2,
+    completeopt = {'menuone', 'noselect'},
+    conceallevel = 0, -- so that `` is visible in markdown files
+    fileencoding = 'utf-8',
+    hlsearch = false,
+    ignorecase = true,
+    mouse = 'a',
+    pumheight = 10,
+    showmode = false,
+    showtabline = 2,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    termguicolors = true,
+    timeoutlen = 100, -- time to wait for a mapped sequence to complete (ms)
+    undofile = true,
+    updatetime = 300, -- faster completion (4000ms default)
+    writebackup = false,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    cursorline = true, -- highlight the current line
+    signcolumn = "yes",
+    scrolloff = 8,
+    sidescrolloff = 8,
+}
+
+vim.opt.shortmess:append "c"
+vim.opt.listchars:append('eol:↴')
+vim.opt.listchars:append('space:⋅')
+
+for k,v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
