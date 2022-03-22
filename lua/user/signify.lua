43e018c577d4c5fe5d3556ad4890da8850e0802c
Cory Omand <comand@pixar.com>
2022-03-22 16:43:19 -0700

    try different signs for add/delete/change.

--- lua/user/signify.lua
+++ lua/user/signify.lua
@@ -1,7 +1,7 @@
-vim.g.signify_sign_add = '┃'
-vim.g.signify_sign_delete = '┃'
-vim.g.signify_sign_delete_first_line = '┃'
-vim.g.signify_sign_change = '┃'
+vim.g.signify_sign_add               = '┃'
+vim.g.signify_sign_delete_first_line = '┳'
+vim.g.signify_sign_delete            = '┋'
+vim.g.signify_sign_change            = '┃'
 vim.g.signify_sign_show_count = false
 
 vim.cmd [[
