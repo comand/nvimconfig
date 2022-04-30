40a4a05e632368f50bcea8dc35972cd348ac71e4
Cory Omand <comand@pixar.com>
2022-04-29 20:47:09 -0700

    Use dawnfox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,21 @@
-vim.cmd [[
-try
-  colorscheme solarized
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-endtry
-]]
 vim.o.background = 'light'
+
+-- local status_ok, sol = pcall(require, 'solarized')
+-- if status_ok then
+--   vim.g.solarized_italic_strings = false
+--   sol.set()
+-- end
+
+local status_ok, nightfox = pcall(require, 'nightfox')
+if status_ok then
+  vim.cmd('colorscheme dawnfox')
+end
+
+vim.cmd [[
+function! SynStack()
+  if !exists("*synstack")
+    return
+  endif
+  echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
+endfunc
+]]
