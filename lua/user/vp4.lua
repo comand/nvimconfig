b1a88617f8f42852b031d71a98cd64ebf9d78ba7
Cory Omand <comand@pixar.com>
2022-05-03 12:27:05 -0700

    Move vp4 mappings into separate file.

---
+++ lua/user/vp4.lua
@@ -0,0 +1,22 @@
+-- Add shortcuts
+local legendary_ok, legendary = pcall(require, 'legendary')
+if legendary_ok then
+  local opts = {noremap = true, silent = true}
+  legendary.bind_keymaps {
+    { '<Leader>pe', '<cmd>Vp4Edit<CR>', opts=opts,
+      description = 'Vp4: open current file for edit',
+    },
+    { '<Leader>pr', '<cmd>Vp4Revert<CR>', opts=opts,
+      description = 'Vp4: revert current file',
+    },
+    { '<Leader>pd', '<cmd>Vp4Diff<CR>', opts=opts,
+      description = 'Vp4: diff current file',
+    },
+    { '<Leader>pi', '<cmd>Vp4Info<CR>', opts=opts,
+      description = 'Vp4: display info for current file',
+    },
+    { '<Leader>ph', '<cmd>Vp4Filelog<CR>', opts=opts,
+      description = 'Vp4: display history for current file',
+    },
+  }
+end
