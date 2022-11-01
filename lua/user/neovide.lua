3bc01b9d289a57b1670efd89b26e0ce3f5c0e6ee
Cory Omand <coryomand@gmail.com>
2022-11-01 14:07:59 -0700

    better config for neovide

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,3 +1,5 @@
--- vim.g['neovide_cursor_animation_length'] = 0.13
--- vim.g['neovide_cursor_trail_length'] = 0.8
--- vim.g['neovide_cursor_vfx_mode'] = 'railgun'
+vim.g['neovide_remember_window_size'] = false
+vim.g['neovide_cursor_animate_in_insert_mode'] = false
+vim.g['neovide_cursor_animate_command_line'] = false
+vim.g['neovide_cursor_vfx_mode'] = 'ripple'
+vim.opt.guifont = 'JetBrainsMono Nerd Font:h11'
