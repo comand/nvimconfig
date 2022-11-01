7944d6b8b4a1919cc2ece9116269685a41194c50
Cory Omand <coryomand@gmail.com>
2022-11-01 14:08:00 -0700

    JetBrainsMono doesn't render properly in neovide.

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -2,4 +2,4 @@ vim.g['neovide_remember_window_size'] = false
 vim.g['neovide_cursor_animate_in_insert_mode'] = false
 vim.g['neovide_cursor_animate_command_line'] = false
 vim.g['neovide_cursor_vfx_mode'] = 'ripple'
-vim.opt.guifont = 'JetBrainsMono Nerd Font:h11'
+vim.opt.guifont = 'Hack Nerd Font:h11'
