05cfdc4b8dad75a540e73c5be9f352b10e0b34fc
Cory Omand <coryomand@gmail.com>
2022-11-01 14:07:59 -0700

    don't require esc to be pressed twice to exit

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -42,6 +42,8 @@ telescope.setup {
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
         ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+
+        ["<esc>"] = actions.close,
       },
 
       n = {
