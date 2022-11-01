a3cabee4255f086cd23f719685ece7a9284cdc43
Cory Omand <coryomand@gmail.com>
2022-11-01 14:07:59 -0700

    looking forward to lua filetype detection

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -45,3 +45,6 @@ vim.opt.guifont = 'JetBrainsMono Nerd Font:h11'
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+-- vim.g['do_file_type_lua'] = 1
+-- vim.g['did_load_filetypes'] = 0
