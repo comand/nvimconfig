dfe87bb49be5bb83899bac76ce14bf8e3c6b8513
Cory Omand <coryomand@gmail.com>
2023-02-28 17:36:00 -0800

    Add startuptime.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim"
   use "nvim-lua/plenary.nvim"
 
+  use 'dstein64/vim-startuptime'
+
   -- Tree sitter
   use {'nvim-treesitter/nvim-treesitter',
     run = function()
