cc5cd8a7a484604710d7f0a9ea2b2e8d3cf8b26f
Cory Omand <coryomand@gmail.com>
2023-03-22 18:34:02 -0700

    Remove unused param.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
       "nvim-lua/plenary.nvim",
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function(_, opts)
+    config = function()
       local telescope = require('telescope')
       local actions = require('telescope.actions')
 
