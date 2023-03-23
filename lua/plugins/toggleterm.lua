0488b1112ccb6bc38568c6c055a559b0cf157f01
Cory Omand <coryomand@gmail.com>
2023-03-22 18:34:10 -0700

    Fix toggleterm setup.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -22,71 +22,63 @@ return {
     },
   },
   keys = {
-    { mode = "t", "<Esc>", "<C-><C-n>", noremap = true },
-    { mode = "t", "<C-h>", "<C-><C-n><C-W>h", noremap = true },
-    { mode = "t", "<C-j>", "<C-><C-n><C-W>j", noremap = true },
-    { mode = "t", "<C-k>", "<C-><C-n><C-W>k", noremap = true },
-    { mode = "t", "<C-l>", "<C-><C-n><C-W>l", noremap = true },
     {
-      "<Leader>TG",
-      "<cmd>lua _LAZYGIT_TOGGLE()<CR>",
-      noremap = true,
-      silent = true,
-      desc = "Open lazygit",
-    },
-    { "<Leader>TD", "<cmd>lua _NCDU_TOGGLE()<CR>", noremap = true, silent = true, desc = "Open ncdu" },
-    { "<Leader>TT", "<cmd>lua _HTOP_TOGGLE()<CR>", noremap = true, silent = true, desc = "Open htop" },
-    {
-      "<Leader>TP",
-      "<cmd>lua _PYTHON_TOGGLE()<CR>",
-      noremap = true,
-      silent = true,
-      desc = "Open python terminal",
+      '<Leader>TG',
+      function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+        lazygit:toggle()
+      end,
+      noremap = true, silent = true, desc = "Open lazygit",
     },
     {
-      "<Leader>TB",
-      "<cmd>ToggleTerm size=10 direction=horizontal<CR>",
-      noremap = true,
-      silent = true,
-      desc = "Open terminal in bottom window",
+      '<Leader>TD',
+      function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+        ncdu:toggle()
+      end,
+      noremap = true, silent = true, desc = 'Open ncdu'
+    },
+    {
+      '<Leader>TT',
+      function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local htop = Terminal:new({ cmd = "htop", hidden = true })
+        htop:toggle()
+      end,
+      noremap = true, silent = true, desc = 'Open htop'
+    },
+    {
+      '<Leader>TP',
+      function()
+        local Terminal = require("toggleterm.terminal").Terminal
+        local python = Terminal:new({ cmd = "python", hidden = true })
+        python:toggle()
+      end,
+      noremap = true, silent = true, desc = 'Open python terminal'
+    },
+    {
+      '<Leader>TB',
+      '<cmd>ToggleTerm size=10 direction=horizontal<CR>',
+      noremap = true, silent = true, desc = 'Open terminal in bottom pane'
     },
   },
+
   config = function(_, opts)
     require("toggleterm").setup(opts)
 
     vim.api.nvim_create_autocmd("TermOpen", {
-      group = "toggleterm",
+      group = vim.api.nvim_create_augroup("toggleterm", { clear = true }),
       pattern = "term://*",
       callback = function()
-        local opts = { noremap = true }
-        vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-        vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+        local kopts = { noremap = true }
+        vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], kopts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], kopts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], kopts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], kopts)
+        vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], kopts)
       end,
     })
-
-    local Terminal = require("toggleterm.terminal").Terminal
-
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-    function _LAZYGIT_TOGGLE()
-      lazygit:toggle()
-    end
-
-    local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-    function _NCDU_TOGGLE()
-      ncdu:toggle()
-    end
-
-    local htop = Terminal:new({ cmd = "htop", hidden = true })
-    function _HTOP_TOGGLE()
-      htop:toggle()
-    end
-
-    local python = Terminal:new({ cmd = "python", hidden = true })
-    function _PYTHON_TOGGLE()
-      python:toggle()
-    end
   end,
 }
