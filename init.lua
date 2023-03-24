2a75c3652a36f7a592aa1ce00b21d9bdcfb9c968
Cory Omand <coryomand@gmail.com>
2023-03-24 12:14:58 -0700

    Minor restructuring of init
    - Move lazy configuration into config/lazy.lua
    - Move color scheme setup into plugins/colorscheme.lua

--- init.lua
+++ init.lua
@@ -1,37 +1,4 @@
--- Set leader -- must happen before plugins are required.
-vim.g.mapleader = ','
-vim.g.maplocalleader = ','
-
--- Plugin Manager Preamble {{{
--- Install package manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-    lazypath,
-  }
-end
-
-vim.opt.rtp:prepend(lazypath)
--- }}}
-
-require('lazy').setup {
-  { -- Color Scheme
-    'EdenEast/nightfox.nvim',
-    priority = 1000,
-    config = function()
-      vim.o.background = 'light'
-      vim.cmd.colorscheme 'dawnfox'
-    end,
-  },
-  { import = 'plugins' },
-  --{ import = 'plugins.site' },
-}
-
-require('config.options')
-require('config.autocommands')
 require('config.keymaps')
+require('config.options')
+require('config.autocmds')
+require('config.lazy')
