1b46741639776830472d013d59ea9445e84b2d87
Cory Omand <coryomand@gmail.com>
2023-03-30 15:01:25 -0700

    Use standard pre_hook for comment/ts_context integration.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -28,23 +28,16 @@ return {
 
   {
     "numToStr/Comment.nvim",
-    event = 'BufReadPost',
-    opts = {
-      pre_hook = function(ctx)
-        local U = require("Comment.utils")
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require("ts_context_commentstring.utils").get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require("ts_context_commentstring.utils").get_visual_start_location()
-        end
-
-        return require("ts_context_commentstring.internal").calculate_commentstring({
-          key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-          location = location,
-        })
-      end,
+    dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+      "JoosepAlviste/nvim-ts-context-commentstring",
     },
+    event = 'BufReadPost',
+    opts = function ()
+      return {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
   },
 
   {
