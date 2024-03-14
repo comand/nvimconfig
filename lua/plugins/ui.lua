96a35bdd0beeff3d6d780b4073b888482805a238
Cory Omand <coryomand@gmail.com>
2024-03-13 17:48:16 -0700

    Removing illuminate keybinds.
    These don't really seem necessary -- when on a term, '*' or '#' works
    well enough to navigate around, and doesn't conflict with LSP bindings.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -172,32 +172,9 @@ return {
     },
     config = function(_, opts)
       require('illuminate').configure(opts)
-
-      local function map(key, dir, buffer)
-        vim.keymap.set('n', key,
-        function()
-          require('illuminate')['goto_' .. dir .. '_reference'](false)
-        end, {
-          desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference",
-          buffer = buffer
-        })
-      end
-
-      map('[[', 'prev')
-      map(']]', 'next')
-
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map('[[', 'prev', buffer)
-          map(']]', 'next', buffer)
-        end,
-      })
     end,
     keys = {
       { "<Tab>i", ":IlluminateToggleBuf<CR>", desc = "Toggle illuminate" },
-      { ']]', desc = 'Next Reference' },
-      { '[[', desc = 'Previous Reference' },
     },
   },
 
