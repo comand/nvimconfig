19b3801d39f69a3fc3806b1e857d628bc181dc1d
Cory Omand <coryomand@gmail.com>
2024-03-22 10:18:32 -0700

    Add neorepl, plugin checkpoint.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -170,4 +170,31 @@ return {
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
+
+  {
+    'ii14/neorepl.nvim',
+    cmd = { 'Repl' },
+    keys = {
+      {
+        '<leader>re',
+        function()
+          -- get current buffer and window
+          local buf = vim.api.nvim_get_current_buf()
+          local win = vim.api.nvim_get_current_win()
+          -- create a new split for the repl
+          vim.cmd('split')
+          -- spawn repl and set the context to our buffer
+          require('neorepl').new{
+            lang = 'vim',
+            buffer = buf,
+            window = win,
+          }
+          -- resize repl window and make it fixed height
+          vim.cmd('resize 10 | setl winfixheight')
+        end,
+        desc = 'Neorepl'
+      },
+    }
+  },
 }
+--
