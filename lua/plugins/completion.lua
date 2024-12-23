5e83d3060151848f83e9825bac0d11e4386e977e
Cory Omand <coryomand@gmail.com>
2024-12-23 12:32:32 -0800

    Fix snippet expansion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -52,22 +52,12 @@ return {
             c = cmp.mapping.close(),
           }),
 
-          ['<CR>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              if luasnip.expandable() then
-                luasnip.expand()
-              else
-                cmp.confirm({select = true})
-              end
-            else
-              fallback()
-            end
-          end),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.locally_jumpable(1) then
+            elseif luasnip.expand_or_locally_jumpable() then
               luasnip.jump(1)
             else
               fallback()
