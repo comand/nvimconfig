4124f1c8924bff9c5feafdd1783658b2e01c9820
Cory Omand <coryomand@gmail.com>
2025-02-27 14:25:58 -0800

    Switch to markview

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -142,11 +142,8 @@ return {
   },
 
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    "OXY2DEV/markview.nvim",
+    lazy = false
   },
-
 }
 --
