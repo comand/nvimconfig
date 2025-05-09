0ac97cee4b048bd0acc5f83c02b0247c009ca1a9
Cory Omand <coryomand@gmail.com>
2025-05-09 16:01:20 -0700

    Switch to new(er) LSP config layout.
    - mason 2.0 + mason-lspconfig
    - after/lsp server configuration
    - no longer supports site lsp configs (need to figure that out again)

---
+++ after/lsp/clangd.lua
@@ -0,0 +1,32 @@
+return {
+  on_attach = function(_, bufnr)
+    vim.keymap.set('n', '<C-H>', ':ClangdSwitchSourceHeader<CR>',
+      { remap = true, silent = true, buffer = bufnr,
+        desc = 'Switch to source/header file' })
+  end,
+  settings = {
+    cmd = {
+      'clangd',
+      '--background-index',
+      '--clang-tidy',
+      '--fallback-style=llvm',
+      '--all-scopes-completion',
+      '--completion-style=detailed',
+      '--header-insertion=iwyu',
+      '--header-insertion-decorators',
+      '--pch-storage=memory',
+      '--enable-config',
+      '--limit-results=0'
+    },
+    filetypes = { 'c', 'cpp', 'cuda', 'proto' },
+    root_markers = {
+      'CMakeLists.txt',
+      '.clangd',
+      '.clang-tidy',
+      '.clang-format',
+      'compile_commands.json',
+      '.git',
+      'SConstruct.options',
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,9 +31,8 @@ return {
     event = {'BufReadPre', 'BufNewFile'},
     dependencies = {
       -- LSP
-      {'williamboman/mason.nvim', cmd = 'Mason',
+      {'mason-org/mason.nvim', cmd = 'Mason',
         config = function() require('mason').setup() end },
-      'williamboman/mason-lspconfig.nvim',
       'p00f/clangd_extensions.nvim',
       {'j-hui/fidget.nvim', opts = {} },
     },
@@ -64,38 +63,6 @@ return {
           prefix = '',
         },
       },
-
-      servers = {
-        bashls = {},
-        clangd = {},
-        jsonls = {},
-        rust_analyzer = {},
-
-        pyright = {
-          python = {
-            analysis = {
-              autoSearchPaths = true,
-              useLibraryCodeForTypes = true,
-              typeCheckingMode = 'off',
-            },
-          },
-        },
-
-        lua_ls = {
-          Lua = {
-            diagnostics = {
-              globals = {'vim'},
-            },
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                [vim.fn.stdpath("config") .. "/lua"] = true,
-              },
-            },
-          },
-        },
-      },
     },
 
     config = function(_, opts)
@@ -116,106 +83,101 @@ return {
         }))
       end
 
-      local on_attach = function(client, bufnr)
-        -- Illuminate
-        require('illuminate').on_attach(client)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-        -- Set the tagfunc to use lsp-definition
-        vim.api.nvim_set_option_value(
-          'tagfunc', 'v:lua.vim.lsp.tagfunc', { buf = bufnr })
-
-        -- Bind LSP keys
-        local map = function(m, lhs, rhs, desc)
-          vim.keymap.set(m, lhs, rhs,
-            { remap = false, silent = true, buffer = bufnr, desc = desc })
-        end
-
-        map('n', '<Tab>d', function()
-            local vtext = not vim.diagnostic.config().virtual_text
-            vim.diagnostic.config({ virtual_text = vtext })
-          end, 'Toggle virtual diagnostic text')
-        map('n', '<Tab>v', function()
-            local vlines = not vim.diagnostic.config().virtual_lines
-            vim.diagnostic.config({ virtual_lines = vlines })
-          end, 'Toggle virtual diagnostic lines')
-
-        map('n', '<leader>rn', vim.lsp.buf.rename, 'LSP: [r]e[n]ame')
-        map('n', '<leader>ca', vim.lsp.buf.code_action, 'LSP: [c]ode [a]ction')
-        map('n', '<leader>cf', vim.lsp.buf.format, 'LSP: [c]ode [f]ormatting')
-        vim.keymap.set('v', '<leader>cf', vim.lsp.buf.format,
-          { silent = true, desc = 'LSP: [c]ode [f]ormatting' })
-
-        map('n', 'K', bordered_hover, 'Show information')
-        map('n', '<C-,>', function()
-            vim.diagnostic.open_float(nil, { scope = 'line' })
-          end, 'Show diagnostics')
-
-        map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
-        map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
-        map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
-        map('n', 'go', vim.lsp.buf.type_definition, 'Go to type definition')
-        map('n', 'gr',
-          require('telescope.builtin').lsp_references, 'Show references')
-        map('n', '<leader>ds',
-          require('telescope.builtin').lsp_document_symbols, 'Document symbols')
-        map('n', '<C-k>', bordered_signature_help, 'Signature documentation')
-      end
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
 
       -- Set up diagnostics.
       vim.diagnostic.config(opts.diagnostic)
 
-      -- LSP config
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      -- Unbind neovim's default LSP keys
+      vim.keymap.del('n', 'grn')
+      vim.keymap.del({'n', 'x'}, 'gra')
+      vim.keymap.del('n', 'grr')
+      vim.keymap.del('n', 'gri')
+      vim.keymap.del('n', 'gO')
+      vim.keymap.del({'i', 's'}, '<C-S>')
 
-      local mason_lspconfig = require('mason-lspconfig')
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(opts.servers)
-      }
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(ev)
+          -- Set the tagfunc to use lsp-definition
+          vim.api.nvim_set_option_value(
+            'tagfunc', 'v:lua.vim.lsp.tagfunc', { buf = ev.buf })
 
-      local make_server_args = function(server_name, default_args)
-        local has_site, site = pcall(require, 'config.site.lsp.' .. server_name)
-        if has_site then
-          return site.make_server_args(default_args)
-        end
-        return default_args
+
+          -- Bind LSP keys
+          local map = function(m, lhs, rhs, desc)
+            vim.keymap.set(m, lhs, rhs,
+              { remap = false, silent = true, buffer = ev.buf, desc = desc })
+          end
+
+          map('n', '<Tab>d', function()
+              local vtext = not vim.diagnostic.config().virtual_text
+              vim.diagnostic.config({ virtual_text = vtext })
+            end, 'Toggle virtual diagnostic text')
+
+          map('n', '<Tab>v', function()
+              local vlines = not vim.diagnostic.config().virtual_lines
+              vim.diagnostic.config({ virtual_lines = vlines })
+            end, 'Toggle virtual diagnostic lines')
+
+          map('n', '<leader>rn', vim.lsp.buf.rename, 'LSP: [r]e[n]ame')
+          -- map('n', '<leader>ca', vim.lsp.buf.code_action, 'LSP: [c]ode [a]ction')
+          map('n', '<leader>ca', function()
+              require('tiny-code-action').code_action()
+            end, 'LSP: [c]ode [a]ction')
+          map('n', '<leader>cf', vim.lsp.buf.format, 'LSP: [c]ode [f]ormatting')
+          map({'n', 'v'}, '<leader>cf', vim.lsp.buf.format, 'LSP: [c]ode [f]ormatting')
+          map('n', 'K', bordered_hover, 'Show information')
+          map('n', '<C-,>', function()
+              vim.diagnostic.open_float(nil, { scope = 'line' })
+            end, 'Show diagnostics')
+
+          map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+          map('n', 'gD', vim.lsp.buf.declaration, 'Go to declaration')
+          map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
+          map('n', 'go', vim.lsp.buf.type_definition, 'Go to type definition')
+          map('n', 'gr',
+            require('telescope.builtin').lsp_references, 'Show references')
+          map('n', '<leader>ds',
+            require('telescope.builtin').lsp_document_symbols, 'Document symbols')
+          map('n', '<C-k>', bordered_signature_help, 'Signature documentation')
       end
-
-      mason_lspconfig.setup_handlers {
-        -- Default config
-        function(server_name)
-          require('lspconfig')[server_name].setup(
-            make_server_args(
-              server_name,
-              {
-                capabilities = capabilities,
-                on_attach = on_attach,
-                settings = opts.servers[server_name],
-                flags = { debounce_text_changes = 150 },
-              }
-            )
-          )
-        end,
-
-        -- Custom configs
-        ['clangd'] = function ()
-          require('lspconfig')['clangd'].setup {
-              cmd = {'clangd', '--enable-config', '--limit-results=0'},
-              capabilities = vim.tbl_extend('keep', capabilities, {
-                offsetEncoding = 'utf-16',
-              }),
-              settings = opts.servers['clangd'],
-              flags = { debounce_text_changes = 150 },
-              on_attach = function(client, bufnr)
-                on_attach(client, bufnr)
-                vim.keymap.set('n', '<C-H>', ':ClangdSwitchSourceHeader<CR>',
-                  { remap = true, silent = true, buffer = bufnr,
-                    desc = 'Switch to source/header file' })
-              end,
-            }
-        end,
-      }
-
+    })
     end,
+  },
+
+  {
+    'mason-org/mason-lspconfig.nvim',
+    event = {'BufReadPre', 'BufNewFile'},
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          "bashls",
+          "clangd",
+          "jsonls",
+          "rust_analyzer",
+          "pyright",
+          "lua_ls",
+        },
+        automatic_enable = true,
+      }
+    end
+  },
+
+  {
+    'rachartier/tiny-code-action.nvim',
+    event = 'LspAttach',
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    opts = {
+      backend = 'difftastic',
+      picker = 'telescope',
+    },
   },
 }
