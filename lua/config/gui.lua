d0be7da08211932f6602680522843fa818a64bb1
Cory Omand <coryomand@gmail.com>
2025-09-05 17:38:39 -0700

    Disable neovide cursor animations.

--- lua/config/gui.lua
+++ lua/config/gui.lua
@@ -8,9 +8,16 @@ if vim.g.neovide then
   vim.g.neovide_floating_z_height = 5
   vim.g.neovide_floating_corner_radius = 0.5
 
-  -- vim.g.neovide_cursor_trail_size = 0
-  vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_hide_mouse_when_typing = true
+
+  -- No cursor animations
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
 
   -- This only works when passing neovide --grid
   vim.opt.lines = 71
