3d3a25d8480dc1522762ed03fa8c0d2e5b488c79
Cory Omand <coryomand@gmail.com>
2025-11-17 12:35:47 -0800

    Turn off floating shadow in neovide.
    This doesn't interact well with some kinds of floating windows,
    particularly the snacks picker.

--- lua/config/gui.lua
+++ lua/config/gui.lua
@@ -1,12 +1,11 @@
 if vim.g.neovide then
+  -- Gui font
   vim.o.guifont = "CommitMono,Symbols_Nerd_Font_Mono:h10.5"
-  vim.g.neovide_remember_window_size = true
-  vim.g.neovide_padding_top = 2
-  vim.g.neovide_padding_bottom = 2
-  -- vim.g.neovide_padding_left = 5
-  -- vim.g.neovide_padding_right = 5
+
+  -- Floating windows
   vim.g.neovide_floating_z_height = 5
   vim.g.neovide_floating_corner_radius = 0.5
+  vim.g.neovide_floating_shadow = false
 
   vim.g.neovide_hide_mouse_when_typing = true
 
@@ -19,6 +18,7 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_scroll_animation_length = 0.00
 
+  vim.g.neovide_remember_window_size = true
   -- This only works when passing neovide --grid
   vim.opt.lines = 71
   vim.opt.columns = 122
